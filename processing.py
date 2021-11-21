51490b864a1e26fae7f6b154d5890b3e1f3a8fe0
Snir Lugassy <snirlugassy@yahoo.com>
2021-11-21 20:14:45 +0200

    faster normalization

--- processing.py
+++ processing.py
@@ -1,29 +1,27 @@
 import string
+import re
 
 import nltk
-from nltk import word_tokenize
+# from nltk import word_tokenize
 from bs4 import BeautifulSoup as bsoup
 
-nltk.download('popular')
+# nltk.download('stopwords')
+# nltk.download('punkt')
 
-NUMERIC_TABLE = str.maketrans('', '', '0123456789')
-PUNC_TABLE = str.maketrans('', '', string.punctuation + '©')
+CHAR_FILTER_TABLE = str.maketrans('', '', string.punctuation + '©' + '0123456789')
 STOPWORDS = nltk.corpus.stopwords.words('english')
+
+WORD_PATTERN = re.compile(r'\w+')
+def tokenize(text):
+    return WORD_PATTERN.findall(text)
 
 def clean_html(text):
     return bsoup(text,'html.parser').get_text()
 
-def remove_numeric(text:str):
-    return text.translate(NUMERIC_TABLE)
-
-def remove_punc(text:str):
-    return text.translate(PUNC_TABLE)
-
 def normalize_text(text):
     text = clean_html(text)
-    text = remove_punc(text)
-    text = remove_numeric(text)
-    tokens = word_tokenize(text)
+    text = text.translate(CHAR_FILTER_TABLE)
+    tokens = tokenize(text)
     tokens = [t for t in tokens if t.lower() not in STOPWORDS]
     tokens = [t for t in tokens if len(t) > 2]
     return tokens
