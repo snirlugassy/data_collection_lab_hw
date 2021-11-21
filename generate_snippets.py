fe241dbe5ea9fb9fda2be18a3d55b865c42a1e47
Snir Lugassy <snirlugassy@yahoo.com>
2021-11-21 18:22:27 +0200

    first commit

---
+++ generate_snippets.py
@@ -0,0 +1,52 @@
+import pickle
+import json
+from collections import defaultdict
+
+import pandas as pd
+import numpy as np
+from sklearn.neural_network import MLPRegressor
+import gensim.downloader as api
+
+from processing import normalize_text
+
+data_file = 'unlabeled.csv'
+
+word2vec = api.load('word2vec-google-news-300')
+
+def w2v(w):
+    try:
+        v = word2vec[w]
+        return v
+    except KeyError:
+        return np.nan
+
+
+if __name__ == '__main__':
+    print('Generating snippets...')
+    data = pd.read_csv(data_file, usecols=['id', 'text'])
+    data['normalized'] = data.text.apply(lambda x: normalize_text(str(x)))
+    data = data.explode('normalized')
+    data.rename(columns={'normalized': 'token'}, inplace=True)
+    data['token_lower'] = data['token'].apply(lambda x:str(x).lower())
+    data.drop_duplicates(subset=['id', 'token_lower'], inplace=True)
+    data['word_vec'] = data['token_lower'].apply(w2v)
+    data.dropna(subset=['word_vec'], inplace=True)
+    data.reset_index(drop=True, inplace=True)
+
+    with open('model_temp.sklearn', 'rb') as model_file:
+        regr = pickle.load(model_file)
+
+    data['score'] = data.word_vec.apply(lambda _v: float(regr.predict(_v.reshape(1,-1))))
+    results = data.groupby(['id'])['score'].nlargest(10)
+
+    output = defaultdict(list)
+
+    for (key, score) in results.items():
+        doc_id = key[0]
+        row_id = key[1]
+        output[doc_id].append(data.iloc[row_id].token)
+
+    json_output = [{'id':key, 'snippet':tokens} for key, tokens in output.items()]
+
+    with open('output.json', 'w') as output_file:
+        json.dump(json_output, output_file)
---
+++ processing.py
@@ -0,0 +1,29 @@
+import string
+
+import nltk
+from nltk import word_tokenize
+from bs4 import BeautifulSoup as bsoup
+
+nltk.download('popular')
+
+NUMERIC_TABLE = str.maketrans('', '', '0123456789')
+PUNC_TABLE = str.maketrans('', '', string.punctuation + '©')
+STOPWORDS = nltk.corpus.stopwords.words('english')
+
+def clean_html(text):
+    return bsoup(text,'html.parser').get_text()
+
+def remove_numeric(text:str):
+    return text.translate(NUMERIC_TABLE)
+
+def remove_punc(text:str):
+    return text.translate(PUNC_TABLE)
+
+def normalize_text(text):
+    text = clean_html(text)
+    text = remove_punc(text)
+    text = remove_numeric(text)
+    tokens = word_tokenize(text)
+    tokens = [t for t in tokens if t.lower() not in STOPWORDS]
+    tokens = [t for t in tokens if len(t) > 1]
+    return tokens
